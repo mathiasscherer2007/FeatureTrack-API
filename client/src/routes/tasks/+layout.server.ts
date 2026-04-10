7cb97d74babdb2be35720f75cc22574527cb2184
Mathias Scherer <mathischerer@gmail.com>
2026-04-10 08:24:27 -0300

    Login should be working now

--- client/src/routes/tasks/+layout.server.ts
+++ client/src/routes/tasks/+layout.server.ts
@@ -1,6 +1,6 @@
 import type { LayoutServerLoad } from '../$types';
 import { PUBLIC_API_URL } from '$env/static/public';
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import { resolve } from 'path';
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
@@ -9,11 +9,14 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application-json',
-				Authorization: `Bearer ${cookies.get('token')}`
+				'Authorization': `Bearer ${cookies.get('token')}`
 			}
 		});
 
 		console.log(response);
+		if (response.status !== 200) {
+			error(response.status);
+		}
 		const result = await response.json();
 		console.log(result);
 	} else {
